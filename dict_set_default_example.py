2e462deba195605d65f6e5520b204053deada47c
kpkrishGit <krpadman@gmail.com>
2020-10-25 14:14:28 -0700

    Update dict_set_default_example.py

--- dict_set_default_example.py
+++ dict_set_default_example.py
@@ -1,12 +1,12 @@
-'''
-find vowel count in a sentence
-We are not initializing a vowels_count with each vowel set to count 0
-'''
-st = 'Here is a sample sentence'
-vowels = ['a', 'e', 'i', 'o', 'u']
-vowel_cnt = {}
-for char in st:
-    if char in vowels:
-        vowel_cnt.setdefault(char, 0)
-        vowel_cnt[char] += 1
-print(vowel_cnt)
+def count_vowels(sentence):
+    """
+    find vowel count in a sentence
+    We are not initializing a vowels_count with each vowel set to 0
+    """
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    vowel_cnt = {}
+    for char in sentence:
+        if char in vowels:
+            vowel_cnt.setdefault(char, 0)
+            vowel_cnt[char] += 1
+    return(vowel_cnt)
